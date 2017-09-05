8da524d6b309e7bbbf3604d720043843210ebbee
baiyang <hubo@youzan.com>
2017-09-05 15:04:46 +0800

    提交

--- src/components/db/ConnectionManager.js
+++ src/components/db/ConnectionManager.js
@@ -1,51 +1,64 @@
+import React, { Component } from 'react'
 import { Layout, Menu, Icon } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
+const {Header, Content, Footer, Sider} = Layout;
 
-ReactDOM.render(<Layout>
-  <Sider
-    breakpoint="lg"
-    collapsedWidth="0"
-    onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
-  >
-    <div className="logo" />
-    <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-      <Menu.Item key="1">
-        <Icon type="user" />
-        <span className="nav-text">nav 1</span>
-      </Menu.Item>
-      <Menu.Item key="2">
-        <Icon type="video-camera" />
-        <span className="nav-text">nav 2</span>
-      </Menu.Item>
-      <Menu.Item key="3">
-        <Icon type="upload" />
-        <span className="nav-text">nav 3</span>
-      </Menu.Item>
-      <Menu.Item key="4">
-        <Icon type="user" />
-        <span className="nav-text">nav 4</span>
-      </Menu.Item>
-    </Menu>
-  </Sider>
-  <Layout>
-    <Header style={{ background: '#fff', padding: 0 }} />
-    <Content style={{ margin: '24px 16px 0' }}>
-      <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-        content
-      </div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>
-      Ant Design ©2016 Created by Ant UED
-    </Footer>
-</Layout>
-</Layout>, mountNode);
+class ConnectionManager extends Component {
 
+  render() {
+    return
+    <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span className="nav-text">nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span className="nav-text">nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span className="nav-text">nav 3</span>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Icon type="user" />
+              <span className="nav-text">nav 4</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{
+            background: '#fff',
+            padding: 0
+          }} />
+          <Content style={{
+            margin: '24px 16px 0'
+          }}>
+            <div style={{
+            padding: 24,
+            background: '#fff',
+            minHeight: 360
+          }}>
+              content
+            </div>
+          </Content>
+          <Footer style={{
+            textAlign: 'center'
+          }}>
+            Ant Design ©2016 Created by Ant UED
+          </Footer>
+        </Layout>
+    </Layout>
+  };
+}
 export default ConnectionManager
 
 
-#components-layout-demo-responsive .logo {
-  height: 32px;
-  background: #333;
-  border-radius: 6px;
-  margin: 16px;
-}
